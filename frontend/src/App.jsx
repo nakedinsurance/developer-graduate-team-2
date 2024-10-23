a98bb28ca984ea67e5f8dcb5f2f8599ae7c75d48
Tapiwa <mazaruratapiwa03@gmail.com>
2024-10-23 17:34:27 +0200

    semi functional

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Checkout } from './components/Checkout/Checkout';
+import ElectronicsStore from './components/E-Store/ElectronicStore';
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/"  index element={<Checkout/>} />
+        <Route path="/"  index element={<ElectronicsStore/>} />
        
       </Routes>
     </div>
