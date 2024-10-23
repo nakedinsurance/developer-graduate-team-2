caba04e0c75b929ed411dbe9ff82f6c4afed80ac
Tapiwa <mazaruratapiwa03@gmail.com>
2024-10-23 14:54:28 +0200

    Basic buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ElectronicsStore from './components/E-Store/ElectronicStore';
 import Wishlist from "./pages/Wishlist"
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+//import InsuranceDetails from './pages/InsuranceDetails';
+import Header from './components/Header';
 
 function App() {
   return (
