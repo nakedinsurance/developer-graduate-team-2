c895e57f688c10ebfceda8ef62ff326b9daa3ad5
Tapiwa <mazaruratapiwa03@gmail.com>
2024-10-23 14:54:28 +0200

    Basic buttons

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import ProductDetails from './pages/ProductDetails';
-//import InsuranceDetails from './pages/InsuranceDetails';
-import Header from './components/Header';
+import { Checkout } from './components/Checkout/Checkout';
 
 function App() {
   return (
     <div>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/"  index element={<Checkout/>} />
+       
       </Routes>
     </div>
   );
