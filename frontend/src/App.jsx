6cf246edb7481ff33b8c9a0d45e110f3fe7f056a
nqobilehlatshwayo <nqobiletheconqueror@gmail.com>
2024-10-24 08:42:59 +0200

    SearchEngine component integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import ElectronicsStore from './components/E-Store/ElectronicStore';
-import Wishlist from "./pages/Wishlist"
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+import SearchEngine from './pages/SearchEngine';
+import Header from './components/Header';
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
         <Route path="/"  index element={<ElectronicsStore/>} />
         <Route path='/wishlist' element = {<Wishlist customerId={"cdad3ffd-f5d6-488e-b76f-a92a151b7c72"}/>}></Route>
         
+        <Route path="/search" element={<SearchEngine />} />
       </Routes>
     </div>
   );
