41cfc574beebd05083eb383a55d397e62a38b430
nqobilehlatshwayo <nqobiletheconqueror@gmail.com>
2024-10-24 08:42:59 +0200

    SearchEngine component integration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import ProductDetails from './pages/ProductDetails';
-//import InsuranceDetails from './pages/InsuranceDetails';
+import SearchEngine from './pages/SearchEngine';
 import Header from './components/Header';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/search" element={<SearchEngine />} />
       </Routes>
     </div>
   );
